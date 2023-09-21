9c3193ed621fb882ff4c7461a57b317f5febff8b
MiniOcean404 <1037306928@qq.com>
2023-09-22 02:19:45 +0800

    feat: 使用 json 库

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include "tty.h"
 #include "env.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 int main() {
+    json ex1 = json::parse(R"(
+      {
+        "pi": 3.141,
+        "happy": true
+      }
+    )");
+    std::cout << ex1.dump() << std::endl;
+
     windows_tty_utf8();
     debug_print();
     std::cout << "你好 世界" << std::endl;
